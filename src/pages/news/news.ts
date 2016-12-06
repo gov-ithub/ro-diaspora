b334453d98345b77a8c951b47fb3bcf70b65014d
Catalin Zalog <c@zalog.ro>
2016-12-06 21:37:51 +0200

    refactor(pages/news): subscription

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -2,21 +2,46 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { News } from '../../models/news';
+
 import { NewsService } from '../../providers/news';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html'
 })
 export class PageNews {
-  news: News[]
+
+  news: News[];
+
+  private subscribeNews: Subscription;
 
   constructor(
     public navCtrl: NavController,
     private newsService: NewsService
-  ) {
-    newsService.load().subscribe(news => {
-      this.news = news;
-    })
+  ) { }
+
+  ionViewWillEnter() {
+    this.setNews();
   }
+
+  ionViewWillLeave() {
+    this.unsubscribe();
+  }
+
+  private setNews() {
+
+    // get news
+    this.subscribeNews = this.newsService.getNews()
+      .subscribe(news => this.news = news);
+
+  }
+
+  private unsubscribe() {
+
+    this.subscribeNews.unsubscribe();
+
+  }
+
 }
