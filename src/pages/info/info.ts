0c751c05a65026b7483697b0dcaf4332b0c8ee71
ClaudiuCeia <claudiu.ceia@gmail.com>
2016-12-09 00:41:59 +0200

    info page

---
+++ src/pages/info/info.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { PageFAQ } from '../faq/faq';
+
+@Component({
+  selector: 'page-info',
+  templateUrl: 'info.html'
+})
+export class PageInfo {
+  FAQPage = PageFAQ;
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello PageInfoPage Page');
+  }
+
+}
