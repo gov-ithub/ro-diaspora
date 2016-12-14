ee0b85bc8dba506fc46de71920b36b18a707b304
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2016-12-15 00:57:48 +0200

    feat(pages/map): Afiseaza puncte de interes in locul sectiilor de vot

--- src/providers/markers.ts
+++ src/providers/markers.ts
@@ -6,10 +6,37 @@ import { VotingStationsData } from './markers-data';
 @Injectable()
 export class MarkersService {
   getMarkers(id?: string): MarkerVotingStation[] {
-    let output: MarkerVotingStation[] = VotingStationsData;
+    let output: MarkerVotingStation[] = VotingStationsData.filter(
+      res => res.category.id !== MarkerCategoryID.SectiiVot  
+    );
+
     if (id) {
       output = output.filter(res => res.id === id);
     }
-    return output;
+    return this.renameMarkers(output);
+  }
+
+  getMarkersByCategoryID(categoryID: MarkerCategoryID): MarkerVotingStation[] {
+    return this.renameMarkers(
+      VotingStationsData.filter(res => res.category.id === categoryID)
+    );
+  }
+
+  getMarkerCategories(): VotingStationCategory[] {
+    let categories = [];
+    VotingStationsData.map((item, index) => {
+      if (categories.indexOf(item.category.id) == -1) {
+        categories[item.category.id] = item.category;
+      }
+    });
+
+    return categories.filter(n => n);
+  }
+
+  private renameMarkers(markers: MarkerVotingStation[]): MarkerVotingStation[] {
+    return markers.map((item, index) => {
+      item.name = item.address;
+      return item;
+    });
   }
 }
